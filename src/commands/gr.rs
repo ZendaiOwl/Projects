7b58b0c76e47eb9aabba42ebeedde0e12ef94389
Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>
2023-07-06 18:32:41 +0200

    ࿓❯ First commit
    Signed-off-by: Victor-ray, S <12261439+ZendaiOwl@users.noreply.github.com>

---
+++ src/commands/gr.rs
@@ -0,0 +1,17 @@
+use rusty::*;
+
+pub fn gr(args: &Vec<String>, writer: impl std::io::Write) {
+    match args.len() {
+        4 => {
+            if Tree::can_open(&std::path::Path::new(&args[3]).to_path_buf()) {
+                Tree::file_search(&args[2], &std::path::Path::new(&args[3]).to_path_buf(), writer);
+            } else {
+                Format::Error(&format!("Unable to open file: {}", &args[3])).print();
+            }
+        },
+        _ => {
+            Format::Error(&format!("Invalid number of arguments: {}/2", args.len()-2)).print();
+            Format::Info("Use: rusty gr [ Pattern ] [ File ]").print();
+        },
+    }
+}
