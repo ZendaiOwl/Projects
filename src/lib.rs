0b40010cc45313143547acdf5824a0d92370e8c7
Victor-ray, S <owl@zendai.net.eu.org>
2023-12-11 22:48:33 +0100

    Squashed 'Rust/dockr_dashboard/' changes from 8062f46..d3a7ee1
    d3a7ee1 𓅓࿓❯ do: Update
    0a8c654 𓅓 ࿓❯ Update
    
    git-subtree-dir: Rust/dockr_dashboard
    git-subtree-split: d3a7ee166ff8d42992c7830b50b19c724f6e9d42

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-#![recursion_limit = "256"]
+// #![recursion_limit = "256"]
 use hyper::{body::HttpBody, Body, Request, Response, StatusCode};
-use std::os::unix::net::UnixStream;
-use std::io::{Read, Write};
-use serde_json::json;
+//use std::os::unix::net::UnixStream;
+//use std::io::{Read, Write};
+//use serde_json::json;
 
 use crate::requests::*;
 use crate::routes::*;
@@ -14,33 +14,3 @@ pub mod routes;
 pub mod requests;
 pub mod handlers;
 pub mod connect;
-    
-#[allow(dead_code)]
-fn read_from_stream(unix_stream: &mut UnixStream) -> Result<(), std::io::Error> {
-    let mut response = String::new();
-    unix_stream
-        .read_to_string(&mut response)
-        .expect("Failed at reading the unix stream");
-    println!("{}", response);
-    Ok(())
-}
-
-#[allow(dead_code)]
-fn write_request_and_shutdown(unix_stream: &mut UnixStream) -> Result<(), std::io::Error> {
-    let json = json!({
-            "Image": "debian:bullseye",
-            "Cmd": [
-                "echo",
-                "hello world"
-            ]
-    });
-    unix_stream
-        .write_all(&serde_json::to_vec(&json).expect("Failed to parse json"))
-        .expect("Failed at writing onto the unix stream");
-    println!("We sent a request");
-    println!("Shutting down writing on the stream, waiting for response...");
-    unix_stream
-        .shutdown(std::net::Shutdown::Write)
-        .expect("Could not shutdown writing on the stream");
-    Ok(())
-}
